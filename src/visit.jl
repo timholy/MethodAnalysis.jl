54b1e6477afafb79ed457f4bc62e6b6852eaba8f
Tim Holy <tim.holy@gmail.com>
2020-05-14 03:43:46 -0500

    Redesign visit_backedges to exploit new visit
    This seems like a cleaner and more robust design.
    We rely on visit to do traversal and have visit_backedges
    focus just on what it knows.

--- src/visit.jl
+++ src/visit.jl
@@ -58,55 +58,55 @@ julia> mods
 [...]
 """
 function visit(@nospecialize(operation); print::Bool=false)
-    visiting = IdSet{Any}()
+    visited = IdSet{Any}()
     for mod in Base.loaded_modules_array()
-        _visit(operation, mod, visiting, print)
+        _visit(operation, mod, visited, print)
     end
     return nothing
 end
 
 # These are non-keyword functions due to https://github.com/JuliaLang/julia/issues/34516
 
-function _visit(@nospecialize(operation), mod::Module, visiting::IdSet{Any}, print::Bool)
-    mod ∈ visiting && return nothing
-    push!(visiting, mod)
+function _visit(@nospecialize(operation), mod::Module, visited::IdSet{Any}, print::Bool)
+    mod ∈ visited && return nothing
+    push!(visited, mod)
     print && println("Module ", mod)
     if operation(mod)
         for nm in names(mod; all=true)
             if isdefined(mod, nm)
                 obj = getfield(mod, nm)
-                _visit(operation, obj, visiting, print)
+                _visit(operation, obj, visited, print)
             end
         end
     end
     return nothing
 end
 
-function _visit(@nospecialize(operation), @nospecialize(f::Callable), visiting::IdSet{Any}, print::Bool)
-    f ∈ visiting && return nothing
-    push!(visiting, f)
+function _visit(@nospecialize(operation), @nospecialize(f::Callable), visited::IdSet{Any}, print::Bool)
+    f ∈ visited && return nothing
+    push!(visited, f)
     print && println("Callable ", f)
     if operation(f)
         ml = methods(f)
-        _visit(operation, ml.mt, visiting, print)
+        _visit(operation, ml.mt, visited, print)
         Base.visit(ml.mt) do m
-            _visit(operation, m, visiting, print)
+            _visit(operation, m, visited, print)
         end
     end
     return nothing
 end
 
-function _visit(@nospecialize(operation), mt::MethodTable, visiting::IdSet{Any}, print::Bool)
-    mt ∈ visiting && return nothing
-    push!(visiting, mt)
+function _visit(@nospecialize(operation), mt::MethodTable, visited::IdSet{Any}, print::Bool)
+    mt ∈ visited && return nothing
+    push!(visited, mt)
     print && println("MethodTable ", mt)
     operation(mt)
     return nothing
 end
 
-function _visit(@nospecialize(operation), m::Method, visiting::IdSet{Any}, print::Bool)
-    m ∈ visiting && return nothing
-    push!(visiting, m)
+function _visit(@nospecialize(operation), m::Method, visited::IdSet{Any}, print::Bool)
+    m ∈ visited && return nothing
+    push!(visited, m)
     print && println("Method ", m)
     if operation(m)
         for fn in (:specializations, :invokes)
@@ -115,10 +115,10 @@ function _visit(@nospecialize(operation), m::Method, visiting::IdSet{Any}, print
                 if spec === nothing
                 elseif isa(spec, Core.TypeMapEntry) || isa(spec, Core.TypeMapLevel)
                     Base.visit(spec) do mi
-                        _visit(operation, mi, visiting, print)
+                        _visit(operation, mi, visited, print)
                     end
                 elseif isa(spec, Core.SimpleVector)
-                    _visit(operation, spec, visiting, print)
+                    _visit(operation, spec, visited, print)
                 else
                     error("unhandled type ", typeof(spec), ": ", spec)
                 end
@@ -128,22 +128,22 @@ function _visit(@nospecialize(operation), m::Method, visiting::IdSet{Any}, print
     return nothing
 end
 
-function _visit(@nospecialize(operation), sv::SimpleVector, visiting::IdSet{Any}, print::Bool)
+function _visit(@nospecialize(operation), sv::SimpleVector, visited::IdSet{Any}, print::Bool)
     for i = 1:length(sv)
         if isassigned(sv, i)
-            _visit(operation, sv[i], visiting, print)
+            _visit(operation, sv[i], visited, print)
         end
     end
     return nothing
 end
 
-function _visit(@nospecialize(operation), mi::MethodInstance, visiting::IdSet{Any}, print::Bool)
-    mi ∈ visiting && return nothing
-    push!(visiting, mi)
+function _visit(@nospecialize(operation), mi::MethodInstance, visited::IdSet{Any}, print::Bool)
+    mi ∈ visited && return nothing
+    push!(visited, mi)
     print && println(mi)
     if operation(mi)
         if isdefined(mi, :cache)
-            _visit(operation, mi.cache, visiting, print)
+            _visit(operation, mi.cache, visited, print)
         end
     end
     return nothing
@@ -163,7 +163,7 @@ if isdefined(Core, :CodeInstance)
     end
 end
 
-_visit(@nospecialize(operation), @nospecialize(x), visiting::IdSet{Any}, print::Bool) = nothing
+_visit(@nospecialize(operation), @nospecialize(x), visited::IdSet{Any}, print::Bool) = nothing
 
 """
     visit_backedges(operation, obj)
@@ -176,61 +176,53 @@ contain `MethodTable`s.
 
 `operation(edge)` should return `true` if the backedges of `edge` should in turn be visited,
 `false` otherwise.
+However, no `MethodInstance` will be visited more than once.
 
 The set of visited objects includes `obj` itself.
 For example, `visit_backedges(operation, f::Function)` will visit all methods of `f`,
 and this in turn will visit all MethodInstances of these methods.
 """
-visit_backedges(operation, obj) =
-    visit_backedges(operation, obj, Set{Union{MethodInstance,MethodTable}}())
-
-function _visit_backedges(operation, mi::MethodInstance, visited)
-    if isdefined(mi, :backedges)
-        for edge in mi.backedges
-            visit_backedges(operation, edge, visited)
-        end
-    end
-    return nothing
+function visit_backedges(@nospecialize(operation), obj)
+    visited = IdSet{MethodInstance}()
+    visit_backedges(operation, obj, visited)
 end
 
-function visit_backedges(operation, mi::MethodInstance, visited)
-    mi ∈ visited && return nothing
-    push!(visited, mi)
-    operation(mi) && _visit_backedges(operation, mi, visited)
-    return nothing
-end
-
-function visit_backedges(operation, mt::MethodTable, visited)
-    mt ∈ visited && return nothing
-    push!(visited, mt)
-    if isdefined(mt, :backedges)
-        for i = 1:2:length(mt.backedges)
-            sig, mi = mt.backedges[i], mt.backedges[i+1]
-            if operation(sig=>mi) && mi ∉ visited
-                push!(visited, mi)
-                _visit_backedges(operation, mi, visited)
+function visit_backedges(@nospecialize(operation), obj, visited::IdSet{MethodInstance})
+    function opwrapper(@nospecialize(x))
+        if isa(x, MethodInstance)
+            _visit_backedges(operation, x, visited)
+            return false
+        elseif isa(x, MethodTable)
+            mt = x::MethodTable
+            if isdefined(mt, :backedges)
+                sigmis = mt.backedges::Vector{Any}
+                for i = 1:2:length(sigmis)
+                    sig, mi = sigmis[i], sigmis[i+1]
+                    _visit_backedges(operation, Pair{Any,MethodInstance}(sig, mi), visited)
+                end
             end
-        end
-    end
-    return nothing
-end
-
-function visit_backedges(operation, m::Method, visited)
-    visit(m) do mi
-        if isa(mi, MethodInstance)
-            visit_backedges(operation, mi, visited)
             return false
         end
-        true
+        return true
+    end
+
+    visit(opwrapper, obj)
+end
+
+function _visit_backedges(@nospecialize(operation), mi::MethodInstance, visited)
+    mi ∈ visited && return nothing
+    push!(visited, mi)
+    if operation(mi) && isdefined(mi, :backedges)
+        for edge in mi.backedges
+            _visit_backedges(operation, edge, visited)
+        end
     end
     return nothing
 end
 
-function visit_backedges(operation, f::Callable, visited)
-    ml = methods(f)
-    visit_backedges(operation, ml.mt, visited)
-    for m in ml
-        visit_backedges(operation, m, visited)
+function _visit_backedges(@nospecialize(operation), misig::Pair{Any,MethodInstance}, visited)
+    if operation(misig)
+        _visit_backedges(operation, misig.second, visited)
     end
     return nothing
 end
